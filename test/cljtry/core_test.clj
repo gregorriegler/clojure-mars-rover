570e959c417993dbaf275affbfc471d4ad47a420
Gregor Riegler <gregor.riegler@ebcont.com>
2019-08-11 23:04:39 +0200

    first rotation

--- test/cljtry/core_test.clj
+++ test/cljtry/core_test.clj
@@ -2,12 +2,16 @@
   (:require [midje.sweet :refer :all]
             [cljtry.core :refer :all]))
 
+(defn rotate-right [position, other-commands]
+  ((land-rover (conj (subvec position 0 2) "E")) other-commands))
+
 (defn move [position, other-commands]
-  (let [new-position (conj (vec (map + position [0, 1])) (last position))]
-    ((create-rover new-position) other-commands)))
+    ((land-rover (conj (vec (map + position [0, 1])) (last position))) other-commands))
 
 (defn exec [position, command, other-commands]
-  (= command "M") (move position other-commands))
+  (cond
+    (= command "M") (move position other-commands)
+    (= command "R") (rotate-right position other-commands)))
 
 (defn land-rover [position]
   (fn [command-string]
@@ -22,3 +26,10 @@
       (rover "M")   => [0, 1, "N"]
       (rover "MM")  => [0, 2, "N"]
       (rover "MMM") => [0, 3, "N"])
+
+(fact "rover rotates"
+      (rover "R")    => [0, 0, "E"]
+    ;  (rover "RR")   => [0, 0, "S"]
+    ;  (rover "RRR")  => [0, 0, "W"]
+    ;  (rover "RRRR") => [0, 0, "N"]
+    )
