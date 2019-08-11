62c2722879f8c8eccadd82302d9ba2ae24994f1c
Gregor Riegler <gregor.riegler@ebcont.com>
2019-08-11 22:47:54 +0200

    adds direction of rover

--- test/cljtry/core_test.clj
+++ test/cljtry/core_test.clj
@@ -3,7 +3,7 @@
             [cljtry.core :refer :all]))
 
 (defn move [position, other-commands]
-  (let [new-position (vec (map + position [0, 1]))]
+  (let [new-position (conj (vec (map + position [0, 1])) (last position))]
     ((create-rover new-position) other-commands)))
 
 (defn exec [position, command, other-commands]
@@ -16,9 +16,9 @@
       (exec position (subs command-string 0 1) (subs command-string 1))
       position)))
 
-(def rover (land-rover [0, 0]))
+(def rover (land-rover [0, 0, "N"]))
 
 (fact "rover moves"
-      (rover "M")   => [0, 1]
-      (rover "MM")  => [0, 2]
-      (rover "MMM") => [0, 3])
+      (rover "M")   => [0, 1, "N"]
+      (rover "MM")  => [0, 2, "N"]
+      (rover "MMM") => [0, 3, "N"])
